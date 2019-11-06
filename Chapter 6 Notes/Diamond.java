53ac587b8ff848c1dce39d769adb9aa6335936af
zehuatan <54189203+zehuatan@users.noreply.github.com>
2019-11-06 15:10:49 -0600

    diamond

--- Chapter 6 Notes/Diamond.java
+++ Chapter 6 Notes/Diamond.java
@@ -10,14 +10,38 @@ public class Diamond
         
         for (int i = 1; i <= side; i++)
         {
-            if ()
+            //side - i = spaces
+            //2i - 1 = asterisks
+            for (int j = 1; j <= side - i; j++)
             {
-                System.out.println("*");
+                System.out.print(" ");
             }
-            else ()
+            for (int k = 1; k <= 2 * i - 1; k++)
             {
-                System.out.println(" ");
+                System.out.print("*");
             }
+            for (int j = 1; j <= side - i; j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+        
+        for (int i = side - 1; i >= 1; i--)
+        {
+            for (int j = 1; j <= side - i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++)
+            {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= side - i; j++)
+            {
+                System.out.print(" ");
+            }
+            System.out.println("");
         }
         
     }
